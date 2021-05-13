7437fbb0fb084c6f964bc00132c773cf76814ff2
Jade Phipps <jadelph@outlook.com>
2021-05-13 23:10:15 +0100

    displaying text but it is undefined

--- main.js
+++ main.js
@@ -16,9 +16,42 @@ async function fetchRecipe(food) {
     const dataResponse = await requestUrl.json();
     console.log(dataResponse);
     const dataResult = dataResponse.hits[0]
-
+    return (dataResult);
 
 };
 
 const YOUR_APP_ID = "81aff1ee"
 const YOUR_APP_KEY = "d92c008e55cda9f4f056523dc8dbd57a"
+
+
+async function displayRecipe() {
+    let recipeObject = await fetchRecipe();
+    let {
+        label,
+        cuisineType,
+        dishType,
+        healthLabels,
+        dietLabels,
+        mealType
+    } = recipeObject;
+
+    let labelEl = document.querySelector(".label");
+    labelEl.innerHTML = label;
+    let cuisineTypeEl = document.querySelector(".cuisine-type");
+    cuisineTypeEl.innerHTML = cuisineType;
+    let dishTypeEl = document.querySelector(".dish-type");
+    dishTypeEl.innerHTML = dishType;
+    let healthLabelsEl = document.querySelector(".health-labels");
+    healthLabelsEl.innerHTML = healthLabels;
+    let dietLabelsEl = document.querySelector(".diet-labels");
+    dietLabelsEl.innerHTML = dietLabels;
+    let mealTypeEl = document.querySelector(".meal-type");
+    mealTypeEl.innerHTML = mealType;
+
+
+
+
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", displayRecipe);
