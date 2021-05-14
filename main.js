93bda0aa0584f90b4a98fe0d66d6b5c5b426cfe3
Jade Phipps <jadelph@outlook.com>
2021-05-14 22:14:37 +0100

    display now working - to add selected objects

--- main.js
+++ main.js
@@ -2,41 +2,41 @@ let foodToSearch = null;
 
 function handleRecipeClick() {
     fetchRecipe(foodToSearch);
-}
+};
 
 function handleFoodChange() {
     foodToSearch = document.querySelector("#food-input").value;
-}
 
-
+};
 const aTag = document.getElementById("recipe-label");
-
 async function fetchRecipe(food) {
     const requestUrl = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
     const dataResponse = await requestUrl.json();
     console.log(dataResponse);
-    const dataResult = dataResponse.hits[0]
-    return (dataResult);
+    const dataResult = dataResponse.hits[0].recipe;
+    console.log(dataResult)
 
+
+    return dataResult;
 };
-
 const YOUR_APP_ID = "81aff1ee"
 const YOUR_APP_KEY = "d92c008e55cda9f4f056523dc8dbd57a"
 
-
 async function displayRecipe() {
-    let recipeObject = await fetchRecipe();
+    foodToSearch = document.querySelector("#food-input").value;
+    let recipeObject = await fetchRecipe(foodToSearch);
     let {
         label,
         cuisineType,
         dishType,
         healthLabels,
         dietLabels,
-        mealType
+        mealType,
+        image,
     } = recipeObject;
-
     let labelEl = document.querySelector(".label");
     labelEl.innerHTML = label;
+    console.log(labelEl)
     let cuisineTypeEl = document.querySelector(".cuisine-type");
     cuisineTypeEl.innerHTML = cuisineType;
     let dishTypeEl = document.querySelector(".dish-type");
@@ -47,11 +47,8 @@ async function displayRecipe() {
     dietLabelsEl.innerHTML = dietLabels;
     let mealTypeEl = document.querySelector(".meal-type");
     mealTypeEl.innerHTML = mealType;
-
-
-
-
+    let imageEl = document.querySelector(".food-image")
+    imageEl.src = image;
 }
-
 let button = document.querySelector("button");
 button.addEventListener("click", displayRecipe);
